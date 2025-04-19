bd31d20364ba5554cf779a6a168ce90efda33e14
Luis Starlino <luis.guilherme009@gmail.com>
2025-04-19 10:49:32 -0300

    feat(command): implemented publish and sendCommand for Banking Microservices

--- MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -4,24 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 
 namespace MicroRabbit.Banking.Application.Services
 {
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepository)
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
             _accountRepository = accountRepository;
+            _bus = bus;
         }
 
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public bool Transfer(AccountTransfer accountTransfer)
+        {
+            // --- Creating a handler command to transfer money
+            var createTransferCommand = new CreateTransferCommand(accountTransfer.FromAccount,accountTransfer.ToAccount, accountTransfer.TransferAmount);
+
+            // --- Send command using core
+            _bus.SendCommand(createTransferCommand);
+
+            return true;
         }
     }
 }
