ad2716a1722bc326de6eb6b747aee77c02e84b67
Luis Starlino <luis.guilherme009@gmail.com>
2025-05-03 08:47:14 -0300

    feat(api): finish the Analytics api controller

--- MicroRabbit.Analytics.Api/Controllers/ClientApprovalController.cs
+++ MicroRabbit.Analytics.Api/Controllers/ClientApprovalController.cs
@@ -1,4 +1,5 @@
-﻿using MicroRabbit.Analytics.Domain.Models;
+﻿using MicroRabbit.Analytics.Application.Interfaces;
+using MicroRabbit.Analytics.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MicroRabbit.Analytics.Api.Controllers
@@ -6,12 +7,46 @@ namespace MicroRabbit.Analytics.Api.Controllers
     [ApiController]
     public class ClientApprovalController : ControllerBase
     {
-        //private readonly
+        private readonly IClientApprovalService _clientApprovalService; 
+
+       public ClientApprovalController(IClientApprovalService clientApprovalService)
+        {
+            _clientApprovalService = clientApprovalService;
+        }
 
         [HttpGet]
         public ActionResult<IEnumerable<ClientApproval>> GetAllClientProcess()
         {
-            return Ok();
+            return Ok(_clientApprovalService.GetAllClientApprovals());
+        }
+
+        [HttpGet]
+        [Route("check-status")]
+        public IActionResult GetApprovalByClientId([FromBody]string clientId)
+        {
+            try
+            {
+                //------------------------------------------------------------------------------------------------
+                // CHECK PARMS
+                //------------------------------------------------------------------------------------------------
+                if(string.IsNullOrEmpty(clientId)) return BadRequest("ERRO02 - Send a valid clientId");
+                var clientNumber = Int32.Parse(clientId);
+
+                //------------------------------------------------------------------------------------------------
+                // GET PROCESS
+                //------------------------------------------------------------------------------------------------
+                var clientApprovalProcess = _clientApprovalService.GetUniqueProcessByClient(clientNumber);
+                if (clientApprovalProcess == null) return Ok("NO DATA");
+                else
+                {
+                    return Ok(clientApprovalProcess);
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest("ERR01 - Internal Server Error");
+            }
         }
     }
 }
