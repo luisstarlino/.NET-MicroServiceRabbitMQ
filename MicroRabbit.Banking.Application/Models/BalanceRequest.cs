372d99924b5f959f4bf5b9f06987896c3db1f842
Luis Starlino <luis.guilherme009@gmail.com>
2025-05-28 22:20:03 -0300

    feat(balance): create a model to balance

---
+++ MicroRabbit.Banking.Application/Models/BalanceRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Application.Models
+{
+    public class BalanceRequest
+    {
+        public decimal Amount { get; set; }
+        public int AccountId { get; set; }
+        public decimal Description { get; set; }
+    }
+}
---
+++ MicroRabbit.Banking.Domain/Models/Balance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.Models
+{
+    public class Balance
+    {
+        [Key]
+        public Guid Id { get; set; } = new Guid();
+        public decimal Amount { get; set; }
+        public decimal Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Foreign Key Configuration
+        public int AccountId { get; set; }
+        public Account Account { get; set; } // EF Navigation to this model    
+    }
+}
